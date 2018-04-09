72a4ae047f324fa87391ebb5dd4b3986e08e281e
Rakiyah Faatuono <just4raki@hotmail.com>
2018-04-08 21:03:50 -0500

    attempt connect local to remote repo

---
+++ remote repo assignment/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Attempt to set up allergen project");
+            Console.WriteLine("First try generated error and I could not push local to remote");
+            Console.ReadLine();
+        }
+    }
+}
