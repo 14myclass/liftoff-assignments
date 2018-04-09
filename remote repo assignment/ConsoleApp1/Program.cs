d01181ff328a7046e3f5fbf12cd38f23fad90260
Rakiyah Faatuono <just4raki@hotmail.com>
2018-04-08 21:38:30 -0500

    STILL TRYING TO CONNECT LOCAL TO REMOTE

--- remote repo assignment/ConsoleApp1/Program.cs
+++ remote repo assignment/ConsoleApp1/Program.cs
@@ -9,6 +9,8 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Attempt to set up allergen project");
             Console.WriteLine("First try generated error and I could not push local to remote");
+            Console.WriteLine("STILL TRYING");
+
             Console.ReadLine();
         }
     }
